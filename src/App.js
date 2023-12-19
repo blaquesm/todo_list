4c5e11a8ea9cb95b63f1b3659adcc39ce6989600
Dmitriy <shulginmma@gmail.com>
2023-12-20 01:57:30 +0300

    style

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import style from './App.module.css';
 
 export const App = () => {
 	const [todo, setTodo] = useState([]);
-	const [isLoadin, setIsLoadin] = useState(false);
+	const [isLoadin, setIsLoadin] = useState(true);
 
 	useEffect(() => {
-		setIsLoadin(true);
-
-		fetch('https://jsonplaceholder.typicode.com/todos')
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=20')
 			.then((loadedData) => loadedData.json())
 			.then((loadedList) => {
-				setTodo(loadedList);
-			})
-			.finally(() => setIsLoadin(false));
+				setTimeout(() => {
+					setTodo(loadedList);
+					setIsLoadin(false);
+				}, 2000);
+			});
 	}, []);
 	return (
 		<div className={style.App}>
+			<div>
+				<h1>Todo list</h1>
+				<hr />
+			</div>
 			{isLoadin ? (
 				<div className={style.loader}></div>
 			) : (
-				todo.map(({ id, title, text }) => (
-					<div key={id} className={style.header}>
-						{id} : {title}
-					</div>
+				todo.map(({ id, title }) => (
+					<ul key={id} className={style.header}>
+						<li className={style.li}>
+							<input type="checkbox" /> {id} {title}
+							<span className={style.rm}>&times;</span>
+						</li>
+					</ul>
 				))
 			)}
 		</div>

1badc7fda568682f08221400a5bdb925c95d7ea2
Dmitriy <shulginmma@gmail.com>
2024-01-01 19:57:24 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ export const App = () => {
 	const [isLoadin, setIsLoadin] = useState(true);
 
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/todos?_limit=20')
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=40')
 			.then((loadedData) => loadedData.json())
 			.then((loadedList) => {
 				setTimeout(() => {
 					setTodo(loadedList);
 					setIsLoadin(false);
-				}, 2000);
+				}, 1000);
 			});
 	}, []);
 	return (
@@ -24,11 +24,12 @@ export const App = () => {
 			{isLoadin ? (
 				<div className={style.loader}></div>
 			) : (
-				todo.map(({ id, title }) => (
+				todo.map(({ id, title, completed }) => (
 					<ul key={id} className={style.header}>
 						<li className={style.li}>
-							<span>
-								<input type="checkbox" /> {id} {title}
+							<span className={completed ? style.done : ''}>
+								<input type="checkbox" checked={completed} readOnly />{' '}
+								{title}
 							</span>
 							<button className={style.rm}>&times;</button>
 						</li>
